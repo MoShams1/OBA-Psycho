791571e64d3d76bd96468aac55ac5f3117f1ff63
MShams <m.shamsahmar@gmail.com>
2023-07-25 01:00:12 +0200

    Almost ready for the test run

--- stimulus/cyc03_task01.py
+++ stimulus/cyc03_task01.py
@@ -17,6 +17,32 @@ import numpy as np
 import pandas as pd
 from psychopy import visual, event, core
 from lib import config_visual as cvis, timestamp, keymouse
+from lib import stimulus_flow_control as sfc
+
+# ----------------------------------------------------------------------------
+
+
+def deg2rad(angle):
+    return angle / 360 * 2 * np.pi
+
+
+def rad2deg(angle):
+    return angle / (2 * np.pi) * 360
+
+
+def pol2cart(rho, phi):
+    phi = deg2rad(phi)
+    x_cart = rho * np.cos(phi)
+    y_cart = rho * np.sin(phi)
+    return x_cart, y_cart
+
+
+def cart2pol(x_cart, y_cart):
+    rho = np.sqrt(x_cart ** 2 + y_cart ** 2)
+    phi = np.arctan2(y_cart, x_cart)
+    phi = rad2deg(phi)
+    return rho, phi
+
 
 # ----------------------------------------------------------------------------
 # turn off Numpy's FutureWarning
@@ -28,7 +54,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 subID = 'test'
 rep_per_cnd = 20  # repetition per condition
 full_screen = False
-running_device = 'mac'  # 'linux' or 'mac'
+running_device = 'linux'  # 'linux' or 'mac'
 
 n_soa = 9
 n_soa_trials = rep_per_cnd * n_soa  # 2/3 of all trials
@@ -69,11 +95,10 @@ im_size = 2
 im_ecc = 6
 # im_opac = .2
 im_opac = .5
-im_dur_ms = 200
-im_dur = int(im_dur_ms / 1000 * frame_rate)  # in frames
+im_dur = int(.25 * frame_rate)
 im1_frames = np.round(np.arange(1, 1.5, .1) * frame_rate)
 im1_frames = im1_frames.astype(int)
-tilt_dur = int(.5 * frame_rate)
+tilt_dur = int(.25 * frame_rate)
 
 att_response_grace_time = int(1 * frame_rate)
 # ----------------------------------------------------------------------------
@@ -113,6 +138,10 @@ keypress_flag = False
 
 timer = core.Clock()
 delta_t = None
+
+resp_eval_arr = []
+run_perf = 80  # set expected accuracy as initial value
+tilt_mag = 30  # set initial tilt
 # ----------------------------------------------------------------------------
 
 # /// START TRIAL ///
@@ -128,9 +157,18 @@ for itrial in range(n_all_trials):
     event_time_s = np.random.choice(np.arange(0, 1.1, .1))
     event_time = int(event_time_s * frame_rate)
 
+    # decide on tilt magnitude
+    if len(resp_eval_arr) >= 10:
+        tilt_change = sfc.cal_next_tilt(goal_perf=80, run_perf=run_perf)
+        tilt_mag = int(tilt_mag + tilt_change)
+    # take care of saturated scenarios
+    if tilt_mag > 99:
+        tilt_mag = 99
+    elif tilt_mag < 1:
+        tilt_mag = 1
+
     # load tilted image
     tilt_cat = np.random.choice(['f', 'h'])
-    tilt_mag = np.random.choice(range(100))
     tilt_dir = np.random.choice(['CW', 'CCW'])
     im_directory = os.path.join(image_folder, 'cyc03_tilted',
                                 f'{tilt_cat}1_tilt{tilt_mag}_{tilt_dir}.png')
@@ -160,42 +198,50 @@ for itrial in range(n_all_trials):
     else:
         cue = h_cnt
 
+    im1_pos = np.nan
+
     # if in soa task
     if soa_task[itrial]:
         # set image times
         im1_frame = event_time
         im2_frame = im1_frame + int(np.absolute(soa_array[itrial]))
         # set image positions
-        im1_pos = im_ecc if soa_array[itrial] > 0 else -im_ecc
-        im2_pos = -im1_pos
+        im1_theta = np.random.choice(np.arange(-45, 45 + 1))
+        if soa_array[itrial] < 0:
+            im1_theta = im1_theta - 180
+        im2_theta = im1_theta + 180
+        im1_pos = pol2cart(im_ecc, im1_theta)
+        im2_pos = pol2cart(im_ecc, im2_theta)
 
         # load peripheral images
-        im_directory = os.path.join(image_folder,  'cyc03_source', 'f1.png')
+        im_directory = os.path.join(image_folder, 'cyc03_source', 'f1.png')
         im1_per = visual.ImageStim(win,
                                    image=im_directory,
                                    size=im_size,
                                    opacity=im_opac,
-                                   pos=(im1_pos, 0))
-        im_directory = os.path.join(image_folder,  'cyc03_source', 'f1.png')
+                                   pos=im1_pos)
+        im_directory = os.path.join(image_folder, 'cyc03_source', 'f1.png')
         im2_per = visual.ImageStim(win,
                                    image=im_directory,
                                    size=im_size,
                                    opacity=im_opac,
-                                   pos=(im2_pos, 0))
+                                   pos=im2_pos)
 
     # -------------------------------
 
     # /// run task
 
     tilt_seen = False
+    pressed_key = []
     correct_resp = np.nan
 
     # gap period
-    for frame in range(int(.5 * frame_rate)):
+    for frame in range(int(1 * frame_rate)):
         win.flip()
 
     # cue period
     for frame in range(int(1 * frame_rate)):
+        cvis.addprobe(win, radius=1.5, color='dimgray', pos=(0, 0))
         cue.draw()
         win.flip()
 
@@ -215,7 +261,7 @@ for itrial in range(n_all_trials):
 
     # soa task
     if soa_task[itrial]:
-        for iframe in range(int(1 * frame_rate)):
+        for iframe in range(im2_frame + im_dur):
             # add central images
             h_cnt.draw()
             f_cnt.draw()
@@ -223,11 +269,11 @@ for itrial in range(n_all_trials):
             cvis.addfixdot(win=win, size=fixdot_size, pos=fixdot_pos,
                            color=fixdot_color)
             # flash peripheral images
-            if iframe >= im1_frame:
+            if (iframe >= im1_frame) and (iframe <= im1_frame + im_dur):
                 im1_per.draw()
                 # if iframe == im1_frame:
                 # timer.reset()
-            if iframe >= im2_frame:
+            if (iframe >= im2_frame) and (iframe <= im2_frame + im_dur):
                 im2_per.draw()
                 # if iframe == im2_frame:
                 # delta_t = np.round(timer.getTime() * 1000)
@@ -257,7 +303,7 @@ for itrial in range(n_all_trials):
         for iframe in range(tilt_time + att_response_grace_time):
             pressed_key = event.getKeys(keyList=['space', 'escape'])
             if (iframe >= tilt_time) and (iframe <= tilt_time + tilt_dur):
-                if cue_array[itrial] == 'f':
+                if tilt_cat == 'f':
                     h_cnt.draw()
                     tilt.draw()
                 else:
@@ -273,14 +319,23 @@ for itrial in range(n_all_trials):
             # check input keys
             if 'space' in pressed_key:
                 tilt_seen = True
-            # evaluate response
-            if (cue_array[itrial] == tilt_cat) and tilt_seen:
-                correct_resp = True
-            else:
-                correct_resp = False
-
             if 'escape' in pressed_key:
                 core.quit()
+
+        # evaluate response
+        if (cue_array[itrial] == tilt_cat) and tilt_seen:
+            correct_resp = True
+        elif (cue_array[itrial] != tilt_cat) and not tilt_seen:
+            correct_resp = True
+        else:
+            correct_resp = False
+        resp_eval_arr.append(correct_resp)
+        run_perf = sum(resp_eval_arr[-10:]) / 10 * 100
+
+        print('=================================')
+        print(f'tilt magnitude: {tilt_mag}')
+        print(f'current eval: {correct_resp}')
+        print(f'running performance: {run_perf}')
 
     # -------------------------------
 
